b0d8207e1c468b9932db027be35ced539f15968d
SebasVZ17 <sebasvz1617@gmail.com>
2026-06-01 22:45:06 -0500

    Error prisma

--- src/db/prisma.ts
+++ src/db/prisma.ts
@@ -3,10 +3,15 @@ import dotenv from 'dotenv'
 
 dotenv.config()
 
+const url = process.env.DATABASE_URL || ''
+const connectionUrl = url.includes('?') 
+  ? url + '&pgbouncer=true&connection_limit=1&prepared_statements=false'
+  : url + '?pgbouncer=true&connection_limit=1&prepared_statements=false'
+
 const prisma = new PrismaClient({
   datasources: {
     db: {
-      url: process.env.DATABASE_URL + '?pgbouncer=true&connection_limit=1&prepared_statements=false',
+      url: connectionUrl,
     },
   },
 })
